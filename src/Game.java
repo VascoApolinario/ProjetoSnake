282f010f6733e04a7ad4a7ffc9ad31aaa3959043
a79809ualg <a79809@ualg.pt>
2024-05-05 14:29:59 +0100

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -18,7 +18,7 @@ public class Game implements ActionListener {
         background = new Background(Width,Height,"Grafico");
         timer = new Timer(100, this);
         running = true;
-        leaderboard = new Leaderboard("\"C:\\Users\\andre\\Desktop\\EngINF\\2º Ano\\POO\\ProjetoSnake\\src\\GameFiles\\leaderboard.csv\"");
+        leaderboard = new Leaderboard("leaderboard.csv");
         System.out.println(leaderboard);
         StartGame();
     }
